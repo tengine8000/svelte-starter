312f0467b22ab90773a7650bfa0cc64ac6a30857
tengine8000 <torty.emmanuel@gmail.com>
2023-11-14 16:29:00 +0100

    WIP: Added github workflow and config changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/svelte-starter/",
+  base: "/website/",
   plugins: [svelte()],
 })
